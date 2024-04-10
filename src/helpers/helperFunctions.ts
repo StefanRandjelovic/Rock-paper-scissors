5f4ec7deb5c0b80239c6f49896c7c7383ba84bc0
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-10 16:33:16 +0200

    Add handleResults function that calculates results.

---
+++ src/helpers/helperFunctions.ts
@@ -0,0 +1,66 @@
+const handleResults = (
+  event: any,
+  setResult: (param: string) => void,
+  setWins: () => void,
+  setLoses: () => void,
+  setTies: () => void,
+  setMessage: (param: any) => void
+) => {
+  const computerMove = Math.floor(Math.random() * 3);
+  const realCM =
+    computerMove === 0
+      ? "rock"
+      : computerMove === 1
+      ? "paper"
+      : computerMove === 2
+      ? "scissors"
+      : null;
+  console.log(event.target.id);
+  if (computerMove === 0 && event.target.id === "rock") {
+    setTies();
+    setResult("tie");
+    setMessage(`You chose ${event.target.id}, computer chose ${realCM} `);
+  }
+  if (computerMove === 1 && event.target.id === "rock") {
+    setLoses();
+    setResult("lose");
+    setMessage(`You chose ${event.target.id}, computer chose ${realCM} `);
+  }
+  if (computerMove === 2 && event.target.id === "rock") {
+    setWins();
+    setResult("win");
+    setMessage(`You chose ${event.target.id}, computer chose ${realCM} `);
+  }
+  if (computerMove === 0 && event.target.id === "paper") {
+    setWins();
+    setResult("win");
+    setMessage(`You chose ${event.target.id}, computer chose ${realCM} `);
+  }
+  if (computerMove === 1 && event.target.id === "paper") {
+    setTies();
+    setResult("tie");
+    setMessage(`You chose ${event.target.id}, computer chose ${realCM} `);
+  }
+  if (computerMove === 2 && event.target.id === "paper") {
+    setLoses();
+    setResult("lose");
+    setMessage(`You chose ${event.target.id}, computer chose ${realCM} `);
+  }
+  if (computerMove === 0 && event.target.id === "scissors") {
+    setLoses();
+    setResult("lose");
+    setMessage(`You chose ${event.target.id}, computer chose ${realCM} `);
+  }
+  if (computerMove === 1 && event.target.id === "scissors") {
+    setWins();
+    setResult("win");
+    setMessage(`You chose ${event.target.id}, computer chose ${realCM} `);
+  }
+  if (computerMove === 2 && event.target.id === "scissors") {
+    setTies();
+    setResult("tie");
+    setMessage(`You chose ${event.target.id}, computer chose ${realCM} `);
+  }
+};
+
+export { handleResults };
