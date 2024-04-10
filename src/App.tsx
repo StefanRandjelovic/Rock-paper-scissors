63a18009471a7be2696677c979d4f554b3418f12
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-10 16:35:55 +0200

    Add local states and move handleResults function to helperFunctions file, add results display and score.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 // DEV DEPENDENCIES
+import { useState } from "react";
+
+// GLOBAL VARIABLES
 import { simpleScore } from "./zustandStore/globalStore";
 
-function App() {
-  const { wins, setWins, loses, setLoses } = simpleScore();
-  // console.log(loses);
-  // console.log(wins);
+// HELPERS
+import { handleResults } from "./helpers/helperFunctions";
 
-  const handleResults = (event: any) => {
-    const computerMove = Math.floor(Math.random() * 3);
-    console.log(computerMove);
-    console.log(event.target.id);
-  };
+function App() {
+  // GLOBAL VARIABLES
+  const { wins, setWins, loses, setLoses, tie, setTies } = simpleScore();
+
+  // LOCAL STATES
+  const [result, setResult] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <>
-      <div onClick={handleResults}>
+      <div
+        onClick={(event) =>
+          handleResults(
+            event,
+            setResult,
+            setWins,
+            setLoses,
+            setTies,
+            setMessage
+          )
+        }
+      >
         <p id="rock">Rock</p>
         <p id="paper">Paper</p>
         <p id="scissors">Scissors</p>
-        
+        <p id="score">
+          {message}
+          {result === "tie" && `the result is ${result}`}
+          {(result === "win" || result === "lose") && `you ${result}`}
+        </p>
+        <p>Wins: {wins}</p>
+        <p>Loses: {loses}</p>
+        <p>Ties: {tie}</p>
       </div>
     </>
   );
