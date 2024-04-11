7ce3d1180e6629929312eb73c14f056ff3fc02e2
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-11 20:58:02 +0200

    Add reset functions to a global state.

--- src/zustandStore/globalStore.ts
+++ src/zustandStore/globalStore.ts
@@ -9,6 +9,9 @@ interface SimpleScore {
   setLoses: () => void;
   tie: number;
   setTies: () => void;
+  resetWins: () => void;
+  resetLoses: () => void;
+  resetTies: () => void;
 }
 
 export const simpleScore = create<SimpleScore>()(
@@ -16,10 +19,13 @@ export const simpleScore = create<SimpleScore>()(
     (set, get) => ({
       wins: 0,
       setWins: () => set({ wins: get().wins + 1 }),
+      resetWins: () => set({ wins: 0 }),
       loses: 0,
       setLoses: () => set({ loses: get().loses + 1 }),
+      resetLoses: () => set({ loses: 0 }),
       tie: 0,
       setTies: () => set({ tie: get().tie + 1 }),
+      resetTies: () => set({ tie: 0 }),
     }),
     { name: "score" }
   )
