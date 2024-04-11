ce393647205fea3493c24074cf2189c3fac84639
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-11 21:00:59 +0200

    Add reset score button, import styles file, add GameCard component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
 // DEV DEPENDENCIES
 import { useState } from "react";
 
+// STYLES
+import "@styles/global.scss";
+
 // GLOBAL VARIABLES
-import { simpleScore } from "./zustandStore/globalStore";
+import { simpleScore } from "@store/globalStore";
 
 // HELPERS
-import { handleResults } from "./helpers/helperFunctions";
+import { handleResults, handleReset } from "@helpers/helperFunctions";
+
+// COMPONENTS
+import GameCard from "./components/gameCard";
 
 function App() {
   // GLOBAL VARIABLES
-  const { wins, setWins, loses, setLoses, tie, setTies } = simpleScore();
+  const {
+    wins,
+    setWins,
+    loses,
+    setLoses,
+    tie,
+    setTies,
+    resetWins,
+    resetLoses,
+    resetTies,
+  } = simpleScore();
 
   // LOCAL STATES
   const [result, setResult] = useState("");
   const [message, setMessage] = useState("");
 
+  const namesArr: string[] = ["rock", "paper", "scissors"];
+
   return (
     <>
       <div
+        className="card-container"
         onClick={(event) =>
           handleResults(
             event,
@@ -29,18 +48,22 @@ function App() {
           )
         }
       >
-        <p id="rock">Rock</p>
-        <p id="paper">Paper</p>
-        <p id="scissors">Scissors</p>
-        <p id="score">
-          {message}
-          {result === "tie" && `the result is ${result}`}
-          {(result === "win" || result === "lose") && `you ${result}`}
-        </p>
-        <p>Wins: {wins}</p>
-        <p>Loses: {loses}</p>
-        <p>Ties: {tie}</p>
+        {namesArr.map((name) => (
+          <GameCard key={crypto.randomUUID()} id={name} />
+        ))}
       </div>
+
+      <p id="score">
+        {message}
+        {result === "tie" && `the result is ${result}`}
+        {(result === "win" || result === "lose") && `you ${result}`}
+      </p>
+      <p>Wins: {wins}</p>
+      <p>Loses: {loses}</p>
+      <p>Ties: {tie}</p>
+      <button onClick={() => handleReset(resetWins, resetLoses, resetTies)}>
+        Reset score
+      </button>
     </>
   );
 }
