8422e63fe86d9cf7963a09d196c05ac37eb82c2c
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-15 21:27:38 +0200

    Import and implement Button and HowTo components.

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,8 @@ import { handleResults, handleReset } from "@helpers/helperFunctions";
 // COMPONENTS
 import GameCard from "./components/gameCard";
 import NavBar from "./components/NavBar";
+import Button from "./components/Button";
+import HowTo from "./components/HowTo";
 
 function App() {
   // GLOBAL VARIABLES
@@ -54,18 +56,22 @@ function App() {
           <GameCard key={crypto.randomUUID()} id={name} />
         ))}
       </div>
-
-      <p id="score">
-        {message ? message : "Play a game to see results."}
-        {result === "tie" && `the result is ${result}`}
-        {(result === "win" || result === "lose") && `you ${result}`}
-      </p>
-      <p>Wins: {wins}</p>
-      <p>Loses: {loses}</p>
-      <p>Ties: {tie}</p>
-      <button onClick={() => handleReset(resetWins, resetLoses, resetTies)}>
-        Reset score
-      </button>
+      <div className="results">
+        <p id="score">
+          {message ? message : "Play a game to see results."}
+          {result === "tie" && `the result is ${result}`}
+          {(result === "win" || result === "lose") && `you ${result}`}
+        </p>
+        <div className="resultsInner">
+          <p>Wins: {wins}</p>
+          <p>Loses: {loses}</p>
+          <p>Ties: {tie}</p>
+        </div>
+        <Button
+          funcParam={() => handleReset(resetWins, resetLoses, resetTies)}
+        />
+      </div>
+      <HowTo />
     </main>
   );
 }
