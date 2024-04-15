d0b2354205bc2022daa1f47c8e0c0e19602958c0
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-15 21:23:48 +0200

    Create HowTo component.

---
+++ src/components/HowTo.tsx
@@ -0,0 +1,13 @@
+// STYLES
+import "@styles/HowTo.scss";
+
+const HowTo = (): React.JSX.Element => {
+  return (
+    <div className="howTo">
+      <p>?</p>
+     
+    </div>
+  );
+};
+
+export default HowTo;
