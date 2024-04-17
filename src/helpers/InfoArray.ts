3735ef44686ca840edcf90ddc256ecfe7b8f759a
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-17 22:05:57 +0200

    Create InfoArray helper file, containing information necessary for the application in the form of an object.

---
+++ src/helpers/InfoArray.ts
@@ -0,0 +1,7 @@
+const infoArrRPS: { readonly title: string; readonly howTo: string } = {
+  title: "Rock, Paper, Scissors game",
+  howTo:
+    "In this game, you choose one of the three options on offer: Rock, paper or scissors. \n Rock beats scissors, but loses to paper; \n Paper beats rock, but loses to scissors; \n And scissors beats paper, but loses to rock. And in case you both chose the same option, then the result is tie.",
+};
+
+export { infoArrRPS };
