5cfd9f02c6f353766602087ed12b2f9d8a7bb0cd
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-21 22:40:55 +0200

    Redo the entire component to serve as the Router hub.

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,26 @@
 // DEV DEPENDENCIES
-import { useEffect, useState } from "react";
-
-// STYLES
-import "@styles/global.scss";
-
-// GLOBAL VARIABLES
-import { simpleScore } from "@store/globalStore";
-
-// HELPERS
-import { handleResults, handleReset } from "@helpers/helperFunctions";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 // COMPONENTS
-import GameCard from "./components/gameCard";
 import NavBar from "./components/NavBar";
-import Button from "./components/Button";
-import HowTo from "./components/HowTo";
-import GameInfo from "./components/GameInfo";
 
-// INFO
-import { infoArrRPS } from "./helpers/InfoArray";
+// PAGES
+import RockPaperScissors from "@/pages/RPS";
+import RockPaperScissorsWoodSaw from "@/pages/RPSWS";
+import CoinToss from "@/pages/CoinToss";
+import HomePage from "@/pages/HomePage";
 
-function App() {
-  // GLOBAL VARIABLES
-  const {
-    wins,
-    setWins,
-    loses,
-    setLoses,
-    tie,
-    setTies,
-    resetWins,
-    resetLoses,
-    resetTies,
-  } = simpleScore();
-
-  // LOCAL STATES
-  const [result, setResult] = useState("");
-  const [message, setMessage] = useState("");
-  const [showHelp, setShowHelp] = useState(false);
-
-  const namesArr: string[] = ["rock", "paper", "scissors"];
-
-  useEffect((): void => {
-    window.addEventListener("keydown", (event: any): void => {
-      if (event.key === "Escape" && showHelp) {
-        setShowHelp(false);
-      }
-    });
-  }, []);
-
+function App(): React.JSX.Element {
   return (
-    <main>
+    <Router>
       <NavBar />
-      <div
-        className="card-container"
-        onClick={(event) =>
-          handleResults(
-            event,
-            setResult,
-            setWins,
-            setLoses,
-            setTies,
-            setMessage
-          )
-        }
-      >
-        {namesArr.map((name) => (
-          <GameCard key={crypto.randomUUID()} id={name} />
-        ))}
-      </div>
-      <div className="results">
-        <p id="score">
-          {message ? message : "Play a game to see results."}
-          {result === "tie" && `the result is ${result}`}
-          {(result === "win" || result === "lose") && `you ${result}`}
-        </p>
-        <div className="resultsInner">
-          <p>Wins: {wins}</p>
-          <p>Loses: {loses}</p>
-          <p>Ties: {tie}</p>
-        </div>
-        <Button
-          funcParam={() => handleReset(resetWins, resetLoses, resetTies)}
-        />
-      </div>
-      <HowTo title="How to play" func={() => setShowHelp(true)} />
-      <GameInfo
-        h1={infoArrRPS.title}
-        p={infoArrRPS.howTo}
-        func={() => setShowHelp(false)}
-        hidShow={showHelp}
-      />
-    </main>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/rps" element={<RockPaperScissors />} />
+        <Route path="/rpsws" element={<RockPaperScissorsWoodSaw />} />
+        <Route path="/coin-toss" element={<CoinToss />} />
+      </Routes>
+    </Router>
   );
 }
 
