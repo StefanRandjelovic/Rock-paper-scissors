040f9ebe04f0d62dea30a7efa5317ade10c13f17
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-21 22:45:00 +0200

    Create RPS page to serve as a Rock, Paper, Scissors game page, moved all the functionality from the App component into this page.

---
+++ src/pages/RPS.tsx
@@ -0,0 +1,98 @@
+// DEV DEPENDENCIES
+import { useEffect, useState } from "react";
+
+// STYLES
+import "@styles/global.scss";
+
+// GLOBAL VARIABLES
+import { simpleScore } from "@store/globalStore";
+
+// HELPERS
+import { handleResults, handleReset } from "@helpers/helperFunctions";
+
+// COMPONENTS
+import GameCard from "@/components/gameCard";
+import Button from "@/components/Button";
+import HowTo from "@/components/HowTo";
+import GameInfo from "@/components/GameInfo";
+
+// INFO
+import { infoArrRPS } from "@/helpers/InfoArray";
+
+const RockPaperScissors = () => {
+
+
+  // GLOBAL VARIABLES
+  const {
+    wins,
+    setWins,
+    loses,
+    setLoses,
+    tie,
+    setTies,
+    resetWins,
+    resetLoses,
+    resetTies,
+  } = simpleScore();
+
+  // LOCAL STATES
+  const [result, setResult] = useState("");
+  const [message, setMessage] = useState("");
+  const [showHelp, setShowHelp] = useState(false);
+
+  const namesArr: string[] = ["rock", "paper", "scissors"];
+
+  useEffect((): void => {
+    window.addEventListener("keydown", (event: any): void => {
+      if (event.key === "Escape" && showHelp) {
+        setShowHelp(false);
+      }
+    });
+  }, []);
+
+  return (
+    <main>
+      <div
+        className="card-container"
+        onClick={(event) =>
+          handleResults(
+            event,
+            setResult,
+            setWins,
+            setLoses,
+            setTies,
+            setMessage
+          )
+        }
+      >
+        {namesArr.map((name) => (
+          <GameCard key={crypto.randomUUID()} id={name} />
+        ))}
+      </div>
+      <div className="results">
+        <p id="score">
+          {message ? message : "Play a game to see results."}
+          {result === "tie" && `the result is ${result}`}
+          {(result === "win" || result === "lose") && `you ${result}`}
+        </p>
+        <div className="resultsInner">
+          <p>Wins: {wins}</p>
+          <p>Loses: {loses}</p>
+          <p>Ties: {tie}</p>
+        </div>
+        <Button
+          funcParam={() => handleReset(resetWins, resetLoses, resetTies)}
+        />
+      </div>
+      <HowTo title="How to play" func={() => setShowHelp(true)} />
+      <GameInfo
+        h1={infoArrRPS.title}
+        p={infoArrRPS.howTo}
+        func={() => setShowHelp(false)}
+        hidShow={showHelp}
+      />
+    </main>
+  );
+};
+
+export default RockPaperScissors;
