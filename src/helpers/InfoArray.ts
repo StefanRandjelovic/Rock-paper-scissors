3e885d1d6323c5dcfae2386bbd27a1f2d33da98c
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-21 22:43:01 +0200

    Add additonal info arrays.

--- src/helpers/InfoArray.ts
+++ src/helpers/InfoArray.ts
@@ -4,4 +4,14 @@ const infoArrRPS: { readonly title: string; readonly howTo: string } = {
     "In this game, you choose one of the three options on offer: Rock, paper or scissors. \n Rock beats scissors, but loses to paper; \n Paper beats rock, but loses to scissors; \n And scissors beats paper, but loses to rock. And in case you both chose the same option, then the result is tie.",
 };
 
-export { infoArrRPS };
+const infoArrRPSWS: { readonly title: string; readonly howTo: string } = {
+  title: "Rock, Paper, Scissors, Wood, Saw game",
+  howTo: "",
+};
+
+const infoArrCT: { readonly title: string; readonly howTo: string } = {
+  title: "Coin-Toss",
+  howTo: "",
+};
+
+export { infoArrRPS, infoArrRPSWS, infoArrCT };
