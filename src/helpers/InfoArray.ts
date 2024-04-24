eb28bd439cdb7658318ce696f141aecb4c43b6b3
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-24 10:00:29 +0200

    Add howTo descriptions to InfoArrRPSWS and infoArrCT arrays.

--- src/helpers/InfoArray.ts
+++ src/helpers/InfoArray.ts
@@ -6,12 +6,14 @@ const infoArrRPS: { readonly title: string; readonly howTo: string } = {
 
 const infoArrRPSWS: { readonly title: string; readonly howTo: string } = {
   title: "Rock, Paper, Scissors, Wood, Saw game",
-  howTo: "",
+  howTo:
+    "In this game, you choose one of the five options on offer: Rock, paper, scissors, wood or saw. \n Rock beats scissors and saw, but loses to paper and wood; \n Paper beats rock and wood, but loses to scissors and saw; \n Scissors beats paper and saw, but loses to rock and wood; \n  Wood beats rock and scissors, but loses to paper and saw; \n Saw beats wood and paper, but loses to rock and scissors. \n And in case you both chose the same option, then the result is tie.",
 };
 
 const infoArrCT: { readonly title: string; readonly howTo: string } = {
   title: "Coin-Toss",
-  howTo: "",
+  howTo:
+    "In this game, you simply choose heads or tails of the coin; \n If your pick matches the result of the coin toss, you win, if it doesn't match, you lose.",
 };
 
 export { infoArrRPS, infoArrRPSWS, infoArrCT };
